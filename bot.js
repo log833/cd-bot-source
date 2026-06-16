670400ea960a1337283418e4065e1591810b6e21
log833 <lcindmakit@gmail.com>
2026-06-15 21:43:45 -0300

    v1.1.8

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const prefix = ",";
 const botname = "Clanker Dask (" + prefix + "help)";
-const version = "V1.1.7";
+const version = "V1.1.8";
 let isPremium = false;
 socket.emit("login", {
         name: botname,
@@ -15,7 +15,7 @@ const help1 = `- %%^^COMMANDS:^^%% \n` +
 `${prefix}help1\\n ${prefix}echo\\n ${prefix}joke\\n ${prefix}triggered\\n ${prefix}fact\\n ${prefix}hat\\n ${prefix}color\\n ${prefix}name\\n ${prefix}resethat\\n ${prefix}resetcolor\\n ${prefix}resetname\\n ${prefix}ship\\n ${prefix}roast\\n ${prefix}cow\\n ${prefix}llama\\n ${prefix}parrot\\n ${prefix}source\\n ${prefix}changelog\\n ${prefix}france\\n ${prefix}asshole\\n ${prefix}lumi\\n ${prefix}gokid`;
 
 const help2 = `- %%^^COMMANDS:^^%% \n` +
-`${prefix}help2\\n ${prefix}pp\\n ${prefix}aplle\\n ${prefix}stfu\\n ${prefix}fuk\\n ${prefix}dvdbounce\\n ${prefix}version\\n ${prefix}coinflip\\n ${prefix}restore\\n ${prefix}repeat\\n ${prefix}apllefy\\n ${prefix}qwertybotify`;
+`${prefix}help2\\n ${prefix}pp\\n ${prefix}aplle\\n ${prefix}stfu\\n ${prefix}fuk\\n ${prefix}dvdbounce\\n ${prefix}version\\n ${prefix}coinflip\\n ${prefix}restore\\n ${prefix}repeat\\n ${prefix}apllefy\\n ${prefix}8ball\\n ${prefix}uptime`;
 
 const changelog = `- https://log833.github.io/cd-bot-source/changelog.html`;
 
@@ -176,10 +176,37 @@ socket.on("talk", (message) => {
         cmd(`hat sprout`);
         cmd(`name Aplle`);
         sendMsg(`**^^MY APLLES!^^**`)
- }   
- if (text === prefix + "qwertybotify") {
-        cmd(`color`);
-        cmd(`name QWERTYBot`);
-        sendMsg(`Q ( - ) W ( - ) E ( - ) R ( - ) T ( - ) Y`)
+ }
+ if (text.startsWith(prefix + "8ball")) {
+    const question = text.substring(prefix.length + 6).trim();
+
+    if (!question) {
+        return sendMsg("Question Plz! Example: ,8ball Example");
+    }
+
+    const answers = [
+        "Yes, Definitely",
+        "No Way",
+        "Maybe",
+        "Ask Again Later",
+        "The Bonzis Says Yes",
+        "IDFK",
+        "100% Confirmed",
+        "Probably Not"
+    ];
+
+    const answer = answers[Math.floor(Math.random() * answers.length)];
+
+    return sendMsg(`${answer}`);
+ }
+ if (text === prefix + "uptime") {
+    const seconds = Math.floor((Date.now() - startTime) / 1000);
+
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+
+    return sendMsg(
+        `Clanker Dask Uptime:\n${hours}h ${minutes % 60}m ${seconds % 60}s`
+    );
  }
 });
